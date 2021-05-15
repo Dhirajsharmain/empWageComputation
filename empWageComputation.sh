6ee3e5a09e1fa35c2b28e1db3be9cdb7787516bd
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-05-15 10:40:39 +0530

    Added: Part time employee and wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,9 +6,13 @@ empRatePerHr=20
 if [ $empCheck -eq 1 ]
 then
 	empHrs=8
+	wage=$(($empRatePerHr*$empHrs))
+	echo "Employee is presen and Wages is : $wage" 
+elif [ $empCheck -eq 2 ]
+then
+	empHrs=4
+	wage=$(($empRatePerHr*$empHrs))
+   echo "Employee is presen and Wages is : $wage"
 else
-	empHrs=0
+	echo "Employee is absent and wages is 0 "
 fi 
-
-wage=$(($empRatePerHr*$empHrs))
-echo "Employee wage : $wage "
