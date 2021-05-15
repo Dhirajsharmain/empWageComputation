c60bb03b1e675763130183fa0a5b552a7bf9dfd4
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-05-15 10:48:01 +0530

    Added: Solving using Switch Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,17 +2,18 @@
 
 empCheck=$((RANDOM%3))
 empRatePerHr=20 
-
-if [ $empCheck -eq 1 ]
-then
-	empHrs=8
-	wage=$(($empRatePerHr*$empHrs))
-	echo "Employee is presen and Wages is : $wage" 
-elif [ $empCheck -eq 2 ]
-then
-	empHrs=4
-	wage=$(($empRatePerHr*$empHrs))
-   echo "Employee is presen and Wages is : $wage"
-else
-	echo "Employee is absent and wages is 0 "
-fi 
+isFullTime=1
+isPartTime=2
+case $empCheck in
+	$isFullTime)
+		empHrs=8 
+	;;
+	$isPartTime)
+		empHrs=4
+	;;
+	*)
+		empHrs=0
+	;;
+esac
+wage=$(($empRatePerHr*$empHrs))
+echo "Wages is : $wage"
