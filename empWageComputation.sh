2e33b6a37ed1580ff3987474ec4061675d6b2e97
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-05-15 11:17:45 +0530

    Added: Store the Day and the Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,6 +9,8 @@ MAX_HRS_IN_MONTHS=50
 totalEmpHrs=0
 totalWorkingDays=0
 
+declare -A empDailyWage
+
 function getWorkingHrs() {
  case $1 in
       $IS_FULL_TIME)
@@ -26,13 +28,13 @@ echo $empHrs
 
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTHS && $totalWorkingDays -lt $NUM_OF_WORKING_DAYS ]]
 do
-	
+	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
 
 	workHours="$( getWorkingHrs $empCheck )"
+   empDailyWage[$totalWorkingDays]=$(( workHours * EMP_RATE_PER_HR ))
 	totalEmpHrs=$(($totalEmpHrs+$workHours))
-   empDailyWage[((totalWorkingDays++))]=$(( workHours * EMP_RATE_PER_HR ))
 done
 wage=$(($EMP_RATE_PER_HR*$totalEmpHrs))
 echo "Wages is : $wage"
-echo ${empDailyWage[@]}
+echo "Every Day Report : " ${empDailyWage[@]}
